de743871485b64cf17b627bd2b33a2d3f41ad1b6
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-02-16 19:50:48 -0800

    :alembic: test click event

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,14 +3,30 @@ import { Links } from '@/components/Links';
 import { NextLogo } from '@/components/NextLogo';
 import { HeadDescription } from '@/components/HeadDescription';
 import { Header } from '@/components/Header';
+import Link from 'next/link';
+import { useCallback } from 'react';
 
 export function Main(props) {
+  
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+  }, []);
+  
   return (
     <main className={styles.main}>
       <Header />
       <HeadDescription title={props.title}>
         <code className={styles.code}>src/pages/{props.title}.js</code>
       </HeadDescription>
+
+      <Link
+        href="/works"
+        onClick={handleClick}
+      >
+        click me
+      </Link>
+
       <NextLogo />
       <Links />
     </main>
