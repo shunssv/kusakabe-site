3d1f243e33cb2c1bfd26af0bfe3bbb48038f15c7
ShunKusakabe <s.kusakabe1124@gmail.com>
2023-03-16 23:54:06 -0700

    :dizzy: add animation for loader - SHU-53

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,11 +1,31 @@
-import styles from '@/components/Loader/Loader.module.scss'
+import styles from '@/components/Loader/Loader.module.scss';
+import { useEffect, useState } from 'react';
 
 export function Loader() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <div className={styles.loaderWrapper}>
-      <div className={styles.loaderContainer}>
-        <span className={styles.loader}></span>
-      </div>
-    </div>
-  )
+    <>
+      {isLoading ? (
+        <div className={styles.loaderWrapper}>
+          <div className={styles.loaderContainer}>
+            <span className={styles.loader}></span>
+          </div>
+        </div>
+      ) : (
+        <div className={`${styles.loaderWrapper} animate__animated animate__fadeOut`}>
+          <div className={styles.loaderContainer}>
+            <span className={styles.loader}></span>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
