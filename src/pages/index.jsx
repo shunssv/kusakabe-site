f296980e3b82054016d4fa6a89953275405129e4
ShunKusakabe <s.kusakabe1124@gmail.com>
2023-03-20 10:18:47 -0700

    :construction: make Layout components - SHU-57

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.scss';
 import { Loader } from '@/components/Loader';
 import { SlWrench } from 'react-icons/sl';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
