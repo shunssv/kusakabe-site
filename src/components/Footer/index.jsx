b66fbd13ca2427f157bf8183a6a852f7b9ba4ff3
ShunKusakabe <s.kusakabe1124@gmail.com>
2023-03-19 18:29:36 -0700

    :recycle: refactor Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,13 +4,18 @@ import { Twitter } from '../Media/Twitter';
 import styles from './Footer.module.scss';
 
 export function Footer() {
-
   return (
     <footer className={styles.footer}>
       <ul role="list" className={styles.snsList}>
-        <li><Twitter url='https://twitter.com/shunssv' /></li>
-        <li><Linkedin url='https://www.linkedin.com/in/shunkusakabe/' /></li>
-        <li><Github url='https://github.com/shunssv' /></li>
+        <li>
+          <Twitter url="https://twitter.com/shunssv" />
+        </li>
+        <li>
+          <Linkedin url="https://www.linkedin.com/in/shunkusakabe/" />
+        </li>
+        <li>
+          <Github url="https://github.com/shunssv" />
+        </li>
       </ul>
       <small className={styles.copyright}>© 2023 shun kusakabe</small>
     </footer>
