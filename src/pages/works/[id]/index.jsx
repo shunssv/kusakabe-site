6642aa1a61a66530cbf96306263eff6b04b950b9
ShunKusakabe <s.kusakabe1124@gmail.com>
2023-03-24 18:18:46 -0700

    :building_construction: make data fetching structure - SHU-70

--- src/pages/works/[id]/index.jsx
+++ src/pages/works/[id]/index.jsx
@@ -3,18 +3,19 @@ import styles from './Works.module.scss';
 import { SlWrench } from 'react-icons/sl';
 import { PrimaryLayout } from '@/components/Layout/PrimaryLayout';
 import { useRouter } from 'next/router';
-import { enkEnglishData } from 'data/Works';
 
-export default function Works() {
+export default function Works(props) {
   const router = useRouter();
   const id = router.query.id;
 
-  console.log(enkEnglishData);
+  const worksList = props.worksList;
+
+  console.log(worksList[0].id);
 
   return (
     <>
       <Head>
-        <title>enk-english_online | Works | shun kusakabe</title>
+        <title>{worksList[0].name} | Works | shun kusakabe</title>
         <meta
           name="description"
           content="This page is for showcases that I have developed so far. I am shun kusakabe."
@@ -24,7 +25,7 @@ export default function Works() {
       <section className={styles.container}>
         <div className={styles.mainteinanceContainer}>
           <SlWrench className={styles.mainteinance} />
-          This is Works page: {id}
+          This is Works page: {worksList[0].name}
           <SlWrench className={styles.mainteinance} />
         </div>
       </section>
@@ -32,6 +33,28 @@ export default function Works() {
   );
 }
 
+//path (id) names
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: 'enk-english' } }, { params: { id: '2' } }],
+    fallback: false,
+  };
+}
+
+//fetching data from the JSON file
+import fsPromises from 'fs/promises';
+import path from 'path';
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data.json');
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData);
+
+  return {
+    props: objectData,
+  };
+}
+
+//layout
 Works.getLayout = function getLayout(page) {
   return <PrimaryLayout>{page}</PrimaryLayout>;
 };
