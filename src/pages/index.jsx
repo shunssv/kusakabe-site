d0f9e934661d266cec1842e4b5766c8917a5f022
ShunKusakabe <s.kusakabe1124@gmail.com>
2023-03-31 15:55:46 -0700

    :sparkles: code contents - SHU-85

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -55,7 +55,6 @@ export default function Home() {
               </dl>
             </Link>
           </section>
-
           <section>
             <Link href="/works/enk-english">
               <h4>
@@ -71,11 +70,25 @@ export default function Home() {
                 <dt>beauty-cream_lp</dt>
                 <dd>assignment</dd>
                 <dd>Vanilla HTML / CSS / JavaScript (jQuery)</dd>
-                <Github url="https://github.com/shunssv/beauty-cream_lp" />
               </dl>
             </Link>
           </section>
         </article>
+
+        <section>
+          <h3>about</h3>
+          <section>
+            <h4>shun kusakabe</h4>
+            <span>he/him</span>
+            <p>web developer / ui designer</p>
+          </section>
+          <div>
+            <p>I make websites and design user interfaces , and am base in Los Angeles, California. Let me introduce myself briefly. I was born and raised in Yamagata which is located at the northern Japan, called Tohoku. After graduating from a university in Tokyo, I have worked for a website agency in Tokyo for approximately four and half year. I am discovering my new career in City of Flowers and Sunshine.</p>
+            <p>If I have to describe myself within one sentence, I always look for new things and dive into them as found. Listening pop music is one of things I love to do in my spare time. I enjoy exploring many songs and artists. Once I found songs which are matched my taste, I listening to them over and over again. Similarly, I would love to learn new something while working as well and make use of them for my work.</p>
+            <p>I believe that my experiences and skills help you out. Please feel free to DM me if you are interested.</p>
+            <p>Thank you for taking the time to read this. Bye for now!</p>
+          </div>
+        </section>
       </div>
     </>
   );
